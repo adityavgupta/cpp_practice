27f511bcd4f89e6abe7e7c2d835fd7a8428be108
Aditya <gupta.adityav@gmail.com>
2019-04-12 00:57:42 -0500

    binary gap and cyclic rotation

---
+++ Binary_gap/Binary_gap_problem.cpp
@@ -0,0 +1,55 @@
+//Program to find the maximum number of 0's in a binary representation of a number
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int BinaryGap (int n) {
+	int setBit = 1;
+	int prev = 0;
+	int i;
+
+	//if there are no 1's or just one 1 return -1 
+	if(n == 0 || (n & (n-1)) == 0)
+		return -1;
+
+	//loop to find the rightmost 1
+	for(i = 1; i <= sizeof(n)*8; i++) {
+		prev++;
+
+		//to find the rightmost bit
+		if ((setBit & n) == setBit) {
+			setBit = setBit << 1;
+			break;
+		}
+
+		//to check next bit
+		setBit = setBit << 1;
+	}
+
+	int max0 = -1;
+	int cur = prev;
+
+	for(int j = i+1; j <= sizeof(n)*8; j++) {
+		cur++;
+
+		//current bit set then compare difference of cur-prev-1 with prev number of maximum zeros
+		if ((n & setBit) == setBit) { 
+			if(max0 < (cur-prev-1)) {
+				max0 = cur-prev-1;
+			}
+			//update prev
+			prev = cur;
+		}
+		setBit = setBit << 1;
+	}
+	return max0;
+}
+
+int main() 
+{ 
+    int n = 560; 
+    // Initially check that number must not 
+    // be 0 and power of 2 
+    std::cout << BinaryGap(n) << std::endl;
+    return 0; 
+} 
---
+++ cyclic_rotation/cyclic_rot.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> &a, int k) {
+    // write your code in C++14 (g++ 6.2.0)
+    
+    //null checks, sanity checks
+    if(k < 0 || a.size() == 0) {
+        return vector<int> ();
+    }
+    if(k == 0 || a.size() == 1) {
+        return a;
+    }
+    
+    //initialize temp and make it the size of a
+    vector<int> temp;
+    temp.resize(a.size());
+    
+    for(int i = a.size()-1; i >= 0; i--) {
+        
+        //if the # of rotation > size
+        if(k > (int) a.size()) {
+            if(i+k >= (int) a.size()) {
+                temp[(i+k-a.size())%a.size()] = a[i];
+            }
+            else {
+                temp[(i+k)%a.size()] = a[i];
+            }
+        }
+        else {
+            if(i+k >= (int) a.size()) {
+                temp[i+k-a.size()] = a[i];
+            }
+            else {
+                temp[i+k] = a[i];
+            }
+        }
+    }
+    return temp;
+}
+
+int main() {
+    std::vector<int> a = {1, 2, 3, 4, 5};
+    int k = 6;
+    std::vector<int> b = solution(a, k);
+
+    for(int i = 0; i < a.size(); i++) {
+        std::cout << a[i] << " ";
+    }
+    std::cout << "" << std::endl;
+    for(int i = 0; i < a.size(); i++) {
+        std::cout << b[i] << " ";
+    }
+        std::cout << "" << std::endl;
+}
